29b40e1ab03ab114d43456d961e20dfa86e65b33
Kurt Vermillion <kurtv0727@gmail.com>
2023-02-19 23:19:48 -0500

    rails validate errors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,20 @@
 class AuthorsController < ApplicationController
   
+def index
+    authors = Author.all
+    render json: authors
+  end
+
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
     author = Author.create(author_params)
-
     render json: author, status: :created
+    rescue ActiveRecord::RecordInvalid => invalid
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
